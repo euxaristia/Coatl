9c0160e22074308dc644120c6d73e452906c550d
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-02-15 23:25:43 -0500

    Remove coatl_subset_to_ir.py and update scripts to use Coatl frontend

--- tests/run_type_i64_smoke.sh
+++ tests/run_type_i64_smoke.sh
@@ -14,7 +14,7 @@ echo "[type-i64] compiling type_i64_smoke.coatl"
 IR="$TMP_DIR/type_i64_smoke.ir"
 WAT="$TMP_DIR/type_i64_smoke.wat"
 
-python3 "$ROOT_DIR/tools/coatl_subset_to_ir.py" "$ROOT_DIR/tests/type_i64_smoke.coatl" -o "$IR"
+"$ROOT_DIR/tools/coatl_subset_to_ir.sh" "$ROOT_DIR/tests/type_i64_smoke.coatl" -o "$IR"
 "$ROOT_DIR/coatl" lower-ir "$IR" -o "$WAT"
 
 out="$(wasmtime --invoke main "$WAT")"
